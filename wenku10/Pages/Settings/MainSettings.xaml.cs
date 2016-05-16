f7a5a008f7223ea9402175effaac3d663f9a59c3
斟酌 鵬兄 <tgckpg@gmail.com>
2016-05-16 18:04:54 +0800

    Experimenting on some design idea

--- wenku10/Pages/Settings/MainSettings.xaml.cs
+++ wenku10/Pages/Settings/MainSettings.xaml.cs
@@ -190,6 +190,7 @@ namespace wenku10.Pages.Settings
                     {
                         new ActionItem( stx.Text( "Help_Wiki"), stx.Text( "Desc_Help_Wiki" ), "https://github.com/tgckpg/wenku10/wiki" )
                         , new ActionItem( stx.Text( "Help_Slack"), stx.Text( "Desc_Help_Slack" ), "https://blog.astropenguin.net/article/view/wenku10-%E7%9A%84%E8%A8%8E%E8%AB%96%E7%B5%84/" )
+                        , new ActionItem( stx.Text( "Help_BugFeature"), stx.Text( "Desc_Help_BugFeature" ), "https://bugzilla.astropenguin.net/" )
                     }
                     , ItemAction = HelpAction
                     , IsEnabled = true
